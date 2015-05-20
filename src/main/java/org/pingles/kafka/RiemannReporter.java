03944303f70596380109950e41aea2e1e8b732b3
Ragnar Dahlén <r.dahlen@gmail.com>
2015-05-20 11:25:00 +0100

    Handle non-numeric gauge values.
    Gauges can have non-numeric values (typically String). Kafka for
    instance exposes it's version number using a gauge. If gauge is not a
    Double, Integer or Long value, just use the String value and use that as
    the state of the event.

--- src/main/java/org/pingles/kafka/RiemannReporter.java
+++ src/main/java/org/pingles/kafka/RiemannReporter.java
@@ -101,11 +101,16 @@ public class RiemannReporter extends AbstractPollingReporter implements MetricPr
         if (gauge.value() instanceof Double) {
             Proto.Event event = builder.setMetricD((Double) gauge.value()).build();
             sendEvent(event);
-        } else {
+        } else if (gauge.value() instanceof Long || gauge.value() instanceof Integer) {
             Long longValue = Long.valueOf(gauge.value().toString());
             Proto.Event event = builder.setMetricSint64(longValue).build();
             sendEvent(event);
+        } else {
+            String value = gauge.value().toString();
+            Proto.Event event = builder.setState(value).build();
+            sendEvent(event);
         }
+
     }
 
     private Proto.Event.Builder buildEvent(String serviceLabel) {
